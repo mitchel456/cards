a8294fdf1a18ca2091660a985c387cb1742630f1
Ryan Mitchell <rmitchell@squareup.com>
2019-05-23 15:24:44 -0700

    create whole sheet

--- tiles.js
+++ tiles.js
@@ -17,23 +17,39 @@ function randomMove() {
   return `${getRandomInt(8, 14)} ${getRandomAbility()}`
 }
 
-const difficulty = getRandomInt(4, 12);
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-ctx.strokeRect(0, 0, 400, 400);
-// 
-// ctx.textAlign = 'center';
-// ctx.textBaseline = 'middle';
-// ctx.font = '50px sans-serif';
-// ctx.fillText(difficulty, 200, 200);
+const cardSize = 400;
+
+// for (i = 0; i < 10; i++) {
+//   for (j = 0; j < 7; j++) {
+//     const difficulty = getRandomInt(4, 12);
+//     const fromLeft = i * cardSize;
+//     const fromTop = j * cardSize;
 //
-// ctx.font = '30px sans-serif';
-// ctx.fillText(randomMove(), 200, 30);
-// ctx.fillText(randomMove(), 200, 370);
+//     ctx.fillStyle = 'white';
+//     ctx.fillRect(i * cardSize, j * cardSize, cardSize, cardSize);
 //
-// ctx.textAlign = 'left';
-// ctx.fillText(randomMove(), 10, 200);
+//     ctx.fillStyle = 'black';
+//     ctx.strokeRect(i * cardSize, j * cardSize, cardSize, cardSize);
 //
-// ctx.textAlign = 'right';
-// ctx.fillText(randomMove(), 390, 200);
+//     ctx.textAlign = 'center';
+//     ctx.textBaseline = 'middle';
+//     ctx.font = '50px sans-serif';
+//     ctx.fillText(difficulty, fromLeft + 200, fromTop + 200);
+//
+//     ctx.font = '30px sans-serif';
+//     ctx.fillText(randomMove(), fromLeft + 200, fromTop + 30);
+//     ctx.fillText(randomMove(), fromLeft + 200, fromTop + 370);
+//
+//     ctx.textAlign = 'left';
+//     ctx.fillText(randomMove(), fromLeft + 10, fromTop + 200);
+//
+//     ctx.textAlign = 'right';
+//     ctx.fillText(randomMove(), fromLeft + 390, fromTop + 200);
+//   }
+// }
+
+// fill in hidden card
+ctx.fillStyle = 'brown';
+ctx.fillRect(0, 0, cardSize, cardSize);
