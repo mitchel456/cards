76afb765399f5beac4e38237275c277b55fe75aa
Ryan Mitchell <mitchellryanj@gmail.com>
2019-05-26 13:47:40 -0700

    Make bonus cards

--- tiles.js
+++ tiles.js
@@ -19,37 +19,45 @@ function randomMove() {
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const cardSize = 400;
+const cardHeight = 400;
+const cardWidth = 280;
+// const numCards = 24;
 
-for (i = 0; i < 10; i++) {
-  for (j = 0; j < 7; j++) {
-    const difficulty = getRandomInt(4, 12);
-    const fromLeft = i * cardSize;
-    const fromTop = j * cardSize;
+$(canvas).height(6 * cardHeight);
+$(canvas).width(6 * cardWidth);
 
+
+for (x = 0; x < 6; x++) {
+  for (y = 0; y < 6; y++) {
+    const bonus = getRandomInt(1, 4);
+    const ability = getRandomAbility();
+    // const difficulty = getRandomInt(4, 12);
+    const fromLeft = x * cardWidth;
+    const fromTop = y * cardHeight;
+    //
     ctx.fillStyle = 'white';
-    ctx.fillRect(i * cardSize, j * cardSize, cardSize, cardSize);
-
+    ctx.fillRect(x * cardWidth, y * cardHeight, cardWidth, cardHeight);
+    //
     ctx.fillStyle = 'black';
-    ctx.strokeRect(i * cardSize, j * cardSize, cardSize, cardSize);
-
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.font = '50px sans-serif';
-    ctx.fillText(difficulty, fromLeft + 200, fromTop + 200);
-
-    ctx.font = '30px sans-serif';
-    ctx.fillText(randomMove(), fromLeft + 200, fromTop + 30);
-    ctx.fillText(randomMove(), fromLeft + 200, fromTop + 370);
-
-    ctx.textAlign = 'left';
-    ctx.fillText(randomMove(), fromLeft + 10, fromTop + 200);
-
-    ctx.textAlign = 'right';
-    ctx.fillText(randomMove(), fromLeft + 390, fromTop + 200);
+    ctx.strokeRect(x * cardWidth, y * cardHeight, cardWidth, cardHeight);
+    //
+    // ctx.textAlign = 'center';
+    // ctx.textBaseline = 'middle';
+    // ctx.font = '50px sans-serif';
+    // ctx.fillText(bonus, fromLeft + 200, fromTop + 200);
+    //
+    // ctx.font = '30px sans-serif';
+    // ctx.fillText(randomMove(), fromLeft + 200, fromTop + 30);
+    // ctx.fillText(randomMove(), fromLeft + 200, fromTop + 370);
+    //
+    // ctx.textAlign = 'left';
+    // ctx.fillText(randomMove(), fromLeft + 10, fromTop + 200);
+    //
+    // ctx.textAlign = 'right';
+    // ctx.fillText(randomMove(), fromLeft + 390, fromTop + 200);
   }
 }
 
-// fill in hidden card
-ctx.fillStyle = 'brown';
-ctx.fillRect(0, 0, cardSize, cardSize);
+// // fill in hidden card
+// ctx.fillStyle = 'brown';
+// ctx.fillRect(0, 0, cardSize, cardSize);
